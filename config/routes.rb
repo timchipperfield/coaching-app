1340c97f9b2f3530e15318f1a8a3273503ec8252
Tim Chipperfield <tim@trueview.me>
2018-06-20 22:32:22 -0700

    exposed devise controllers and began editing registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+          sessions: 'users/sessions',
+          passwords: 'users/passwords',
+          registrations: 'users/registrations',
+          unlocks: 'users/unlocks'
+        }
+
   root to: "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
